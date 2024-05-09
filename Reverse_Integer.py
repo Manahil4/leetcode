d48c4166295b8f674176c4db51171592e1e17830
Manahil4 <142149713+Manahil4@users.noreply.github.com>
2024-05-09 08:39:28 -0700

    Update Reverse_Integer.py
    made some changes but till not correct code

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -8,10 +8,11 @@ class Solution(object):
         # Loop until the input integer becomes zero
         while x != 0:
             digit = abs(x) % 10  # Extract the last digit of the input integer
+            
             # Check if multiplying the current answer by 10 and adding the last digit will cause overflow
-            if (x > (Max // 10)) or (x < (Min // 10)):
+            if (ans > (Max // 10)) | (ans < (Min // 10) ):
                 return 0  # If overflow is detected, return 0
-                
+            
             ans = (ans * 10) + digit  # Add the last digit to the answer after shifting it left by one position
             x = x // 10  # Remove the last digit from the input integer using integer division
         if x < 0:
