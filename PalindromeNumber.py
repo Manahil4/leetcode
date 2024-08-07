8f3f1e0cdd0693dce055f3df5daf0b873c11be17
Manahil4 <142149713+Manahil4@users.noreply.github.com>
2024-08-08 00:27:03 +0500

    Update PalindromeNumber.py

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -8,3 +8,8 @@ class Solution(object):
             return False
         str_x=str(x)
         return str_x==str_x[::-1]
+# Conversion to String: str_x = str(x) converts the integer x to its string representation.
+# Reversal of String: str_x[::-1] creates a new string that is the reverse of str_x. The slice notation [::-1] means:
+# : indicates the full range of the string.
+# The first : means to include all characters.
+# The second : followed by -1 indicates the step, i.e., traverse the string from end to start.
