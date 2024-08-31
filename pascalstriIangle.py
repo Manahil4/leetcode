4253c3be75cb1c5b451caa4f1e7fe4f3f4783228
Manahil4 <142149713+Manahil4@users.noreply.github.com>
2024-08-31 23:09:16 +0500

    Update pascalstriIangle.py

--- pascalstriIangle.py
+++ pascalstriIangle.py
@@ -1,8 +1,9 @@
 n8=int( input("number to repeat pattern" ))
-rowl=[]
-res=[]
+
+res=[[1]]
 temp=[]
 for row in range(1,n8+1) :
+    rowl=[]
     temp=[0]+res[-1]+[0]
     for col in range (len(res[-1])+1 ):
         rowl.append(temp[col]+temp[col+1])
